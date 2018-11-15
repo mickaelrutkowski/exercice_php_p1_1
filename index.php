49c9a0300fbb6c493334ff87fa3ee608e66d28ce
mickaelrutkowski <mickael.rutkowski@gmail.com>
2018-11-15 14:30:24 +0100

    php exo1

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP</title>
+  </head>
+  <body>
+  <?php echo "Cette ligne a été écrite \"uniquement\" en PHP."; ?>
+     <p>Aujourd'hui nous sommes le <?php echo date('d/m/Y h:i:s'); ?>.</p>
+     <?php
+echo "J'habite en Chine."; // Cette ligne indique où j'habite
+// La ligne suivante indique mon âge
+echo "J'ai 92 ans.";
+?>
+  </body>
+</html>
